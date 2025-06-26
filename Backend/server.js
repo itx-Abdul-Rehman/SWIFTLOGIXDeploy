67932635d9c81376f3ce3b1aa497071504431cac
AbdulRehman <itsrehman2004@gmail.com>
2025-06-26 17:45:39 +0500

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -69,7 +69,7 @@ app.use('/api', EmployeeRoute);
 app.use('/api', InsuranceRoute);
 
 
-app.get('/home', (req, res) => {
+app.get('/api/home', (req, res) => {
   res.send("Backend running....")
 });
 
